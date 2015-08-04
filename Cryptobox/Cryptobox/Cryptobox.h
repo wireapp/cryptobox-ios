45ea139784dc35b265827db8e22a0d540bab5bb8
Andreas Kompanez <andreas.kompanez@wearezeta.com>
2015-08-04 16:59:04 +0200

    Initial commit after Xcode project creation

---
+++ Cryptobox/Cryptobox/Cryptobox.h
@@ -0,0 +1,13 @@
+//
+//  Cryptobox.h
+//  Cryptobox
+//
+//  Created by Andreas Kompanez on 04.08.15.
+//  Copyright (c) 2015 Cryptobox. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Cryptobox : NSObject
+
+@end
---
+++ Cryptobox/Cryptobox/Cryptobox.m
@@ -0,0 +1,13 @@
+//
+//  Cryptobox.m
+//  Cryptobox
+//
+//  Created by Andreas Kompanez on 04.08.15.
+//  Copyright (c) 2015 Cryptobox. All rights reserved.
+//
+
+#import "Cryptobox.h"
+
+@implementation Cryptobox
+
+@end
