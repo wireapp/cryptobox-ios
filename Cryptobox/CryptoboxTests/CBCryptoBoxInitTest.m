da70d74c8a1fffb68ae09655304e7470b602fdae
Andreas Kompanez <andreas.kompanez@wearezeta.com>
2015-08-12 15:30:16 +0200

    Better naming

--- Cryptobox/CryptoboxTests/CBCryptoBoxInitTest.m
+++ Cryptobox/CryptoboxTests/CBCryptoBoxInitTest.m
@@ -11,24 +11,11 @@
 
 #import "Cryptobox.h"
 
-@interface CBCryptoBoxTests : XCTestCase
+@interface CBCryptoBoxInitTest : XCTestCase
 
 @end
 
-@implementation CBCryptoBoxTests
-
-- (void)setUp
-{
-    [super setUp];
-    
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown
-{
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
+@implementation CBCryptoBoxInitTest
 
 - (void)testThatCryptoBoxInitWithPathWorks
 {
