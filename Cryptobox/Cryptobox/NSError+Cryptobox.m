94ffbf9a0da5c613b8ae5dca82cabd000613ed7e
Andreas Kompanez <andreas.kompanez@wire.com>
2015-08-18 12:05:17 +0200

    Header update

--- Cryptobox/Cryptobox/NSError+Cryptobox.m
+++ Cryptobox/Cryptobox/NSError+Cryptobox.m
@@ -1,10 +1,7 @@
-//
-//  NSError+Cryptobox.m
-//  Cryptobox
-//
-//  Created by Andreas Kompanez on 04.08.15.
-//  Copyright (c) 2015 Cryptobox. All rights reserved.
-//
+// This Source Code Form is subject to the terms of
+// the Mozilla Public License, v. 2.0. If a copy of
+// the MPL was not distributed with this file, You
+// can obtain one at http://mozilla.org/MPL/2.0/.
 
 #import "NSError+Cryptobox.h"
 
