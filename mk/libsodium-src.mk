6a2c63eae1c746209fd0cc8ed6cecbaa61b7d215
Jacob Persson <jacob.persson@gmail.com>
2019-06-13 17:34:03 +0200

    Bump libsodium to 1.0.16

--- mk/libsodium-src.mk
+++ mk/libsodium-src.mk
@@ -1,4 +1,4 @@
-LIBSODIUM_VERSION := 1.0.14
+LIBSODIUM_VERSION := 1.0.16
 LIBSODIUM         := libsodium-$(LIBSODIUM_VERSION)
 LIBSODIUM_URL     := http://download.libsodium.org/libsodium/releases/$(LIBSODIUM).tar.gz
 LIBSODIUM_SRC     := build/src/$(LIBSODIUM)
