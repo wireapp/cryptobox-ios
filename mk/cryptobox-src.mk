e148a5a195bfe57399bf6a961008d46d1390022a
Bill Chan <billypchan@gmail.com>
2022-01-21 07:07:51 +0100

    fix github url

--- mk/cryptobox-src.mk
+++ mk/cryptobox-src.mk
@@ -1,6 +1,6 @@
 CRYPTOBOX_VERSION := v1.1.1
 CRYPTOBOX         := cryptobox-$(CRYPTOBOX_VERSION)
-CRYPTOBOX_GIT_URL := git@github.com:wireapp/cryptobox-c.git
+CRYPTOBOX_GIT_URL := https://github.com/wireapp/cryptobox-c.git
 CRYPTOBOX_SRC     := build/src/$(CRYPTOBOX)
 
 $(CRYPTOBOX_SRC):
